19821ecf2d5c687bba85077d1c24feae0aa25c00
Nir Weingarten <nirzvi89@gmail.com>
2022-04-02 18:47:23 +0300

    added load functionality to experiment.py

--- experiment.py
+++ experiment.py
@@ -6,6 +6,10 @@ from models import MultiModalCONVLSTMActorCritic, CONVLSTMActorCritic
 from a2c_agent import A2CAgent
 import utils
 from datetime import datetime
+import argparse
+import pickle
+import retro
+from utils import Discretizer
 
 DEVICE = torch.device('cuda:3')
 # DEVICE = torch.device('cuda')
@@ -14,13 +18,8 @@ DEVICE = torch.device('cuda:3')
 timestamp = datetime.now().strftime('%y%m%d%H%m%s')
 SAVE_DIR = '/home/nir/stereo_augmented_rl/saved_agents/experiment_{}'.format(timestamp)
 # SAVE_DIR = '/users/nirweingarten/Desktop/tuning_{}'.format(timestamp)
-# SAVE_DIR = '/content/drive/MyDrive/RL_research/skel_plus/saved_models/'
-if not os.path.exists(SAVE_DIR):
-    try:
-        os.makedirs(SAVE_DIR)
-    except FileExistsError:
-        sys.stdout.write('Directory already exists\n')
-        sys.stdout.flush()
+# SAVE_DIR = '/content/drive/MyDrive/RL_research/skel_plus/saved_models/experiment_{}'.format(timestamp)
+
 EPOCHS = 3000
 TRAJ_LEN = 50000
 FINAL_EXP_TIME = 1000000
@@ -47,28 +46,28 @@ class ExperimentWorker():
     """
     A multiprocess worker that runs one type of agent 3 times
     """
-    def __init__(self, obs_type, save_dir, num_runs=3):
+    def __init__(self, obs_type, save_dir, num_runs=1):
         self.obs_type = obs_type
         self.save_dir = save_dir
         self.num_runs = num_runs
-        self.dir_path = os.path.join(save_dir, obs_type)
-        if not os.path.isdir(self.dir_path):
-            os.mkdir(self.dir_path)
+        self.save_dir = os.path.join(save_dir, obs_type)
+        if not os.path.isdir(self.save_dir):
+            os.mkdir(self.save_dir)
         self.env = utils.EnvWrapper('skeleton_plus', utils.ObsType[obs_type],
                                     utils.ActionType['ACT_WAIT'], max_steps=MAX_STEPS, num_discrete=10, debug=False,
                                     time_penalty=0.0, frames_to_skip=FRAMES_TO_SKIP, use_history=USE_HISTORY)
 
-    def run(self):
+    def run(self, device):
         for run in range(self.num_runs):
             agent_name = 'run_{}'.format(run)
             save_path = os.path.join(self.save_dir, agent_name)
             log_path = 'logs/{}.log'.format(agent_name)
-            if obs_type in MULTIMODAL_OBS_TYPES:
-                model = MultiModalCONVLSTMActorCritic(self.env.obs_shape, self.env.num_actions, device=DEVICE, hidden_size=HIDDEN_SIZE, num_lstm_layers=NUM_LSTM_LAYERS)
+            if self.obs_type in MULTIMODAL_OBS_TYPES:
+                model = MultiModalCONVLSTMActorCritic(self.env.obs_shape, self.env.num_actions, device=device, hidden_size=HIDDEN_SIZE, num_lstm_layers=NUM_LSTM_LAYERS)
                 sys.stdout.write('Chose multi-modal model\n')
                 sys.stdout.flush()
             else:
-                model = CONVLSTMActorCritic(self.env.obs_shape, self.env.num_actions, device=DEVICE, hidden_size=HIDDEN_SIZE, num_lstm_layers=NUM_LSTM_LAYERS)
+                model = CONVLSTMActorCritic(self.env.obs_shape, self.env.num_actions, device=device, hidden_size=HIDDEN_SIZE, num_lstm_layers=NUM_LSTM_LAYERS)
                 sys.stdout.write('Chose single-modal model\n')
                 sys.stdout.flush()
 
@@ -79,7 +78,7 @@ class ExperimentWorker():
             agent.train(epochs=EPOCHS, trajectory_len=TRAJ_LEN, env_wrapper=self.env,
                     lr=LR, discount_gamma=GAMMA, scheduler_gamma=SCHED_GAMMA, beta=BETA,
                     print_interval=PRINT_INTERVAL, log_interval=LOG_INTERVAL, scheduler_interval=SCHED_INTERVAL,
-                    epsilon=EPSILON, batch_size=32, epsilon_min=0.1, device=DEVICE, replay_init_len=REPLAY_INIT_LEN,
+                    epsilon=EPSILON, batch_size=32, epsilon_min=0.1, device=device, replay_init_len=REPLAY_INIT_LEN,
                     final_exp_time=FINAL_EXP_TIME, save_interval=SAVE_INTERVAL)
             utils.save_agent(agent)
             end = time.time()
@@ -90,12 +89,58 @@ class ExperimentWorker():
         sys.stdout.flush()
 
 
-if __name__ == '__main__':
-    if torch.cuda.is_available() and (DEVICE == torch.device('cuda')):
-        sys.stdout.write('Using CUDA {}\n'.format(DEVICE))
+def main(raw_args):
+    parser = argparse.ArgumentParser(description='Creates or load an agent and then trains it')
+    parser.add_argument(
+        '-load', type=str, nargs='?', help='Weather or not to load an existing agent from the specified path.\n'
+                                           'In case of loading all other arguments are ignored', default='')
+    parser.add_argument(
+        '-device', type=str, nargs='?', help='Device type: cpu, cuda, cuda:0, cuda:1,...', default='cuda')
+    parser.add_argument(
+        '-epochs', type=int, nargs='?', help='How many epochs to train', default=0)
+    parser.add_argument(
+        '-save_path', type=str, nargs='?', help='new save path for loaded agent. Defaults to old path', default='')
+    parser.add_argument(
+        '-obs_type', type=str, nargs='?', help='obs type for new experiment', default='')
+    args = parser.parse_args(raw_args)
+
+    if torch.cuda.is_available() and ('cuda' in args.device):
+        sys.stdout.write('Using CUDA {}\n'.format(args.device))
     else:
         sys.stdout.write('Using CPU\n')
     sys.stdout.flush()
-    obs_type = sys.argv[1]
-    worker = ExperimentWorker(obs_type, SAVE_DIR)
-    worker.run()
+
+    # import ipdb
+    # ipdb.set_trace()
+
+    if args.load:
+        assert os.path.isfile(args.load)
+        with open(args.load, 'rb') as f:
+            agent = pickle.load(f)
+            # In retro one cannot save the env and discretisizer, so we need to recreate them
+            # agent.env.env = retro.retro_env.RetroEnv
+            agent.env.env = retro.make(game='skeleton_plus', inttype=retro.data.Integrations.ALL)
+            agent.env.discretisizer = Discretizer(agent.env.env, [['UP'], ['LEFT'], ['RIGHT'], ['BUTTON'], [None]])
+            if args.save_path:
+                assert os.path.isdir(os.path.dirname(args.save_path))
+                agent.save_path = args.save_path
+            sys.stdout.write(f'agent trained for {len(agent.all_rewards)} episodes\nFinal episode scored {agent.all_rewards[-1]}\nObs type: {agent.env.obs_type}\n')
+            sys.stdout.flush()
+            agent.train(epochs=args.epochs, trajectory_len=TRAJ_LEN, env_wrapper=agent.env, lr=LR, discount_gamma=GAMMA,
+                        scheduler_gamma=SCHED_GAMMA, beta=BETA, print_interval=PRINT_INTERVAL,
+                        log_interval=LOG_INTERVAL, save_interval=SAVE_INTERVAL, scheduler_interval=SCHED_INTERVAL,
+                        clip_gradient=False, eval_interval=0, device=torch.device(args.device))
+    else:
+        if not os.path.exists(SAVE_DIR):
+            try:
+                os.makedirs(SAVE_DIR)
+            except FileExistsError:
+                sys.stdout.write('Directory already exists\n')
+                sys.stdout.flush()
+
+        worker = ExperimentWorker(args.obs_type, SAVE_DIR)
+        worker.run(torch.device(args.device))
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
